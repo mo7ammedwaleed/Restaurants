bdd0a8a4ec3a904c610aa4ceb53a3987f4a4fd14
Mohammed Waleed <mwaleedx1@gmail.com>
2025-09-26 18:21:36 +0300

    Time logging middleware

---
+++ Restaurants.API/CustomMiddlewares/RequestTimeLoggingMiddleware.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Diagnostics;
+
+namespace Restaurants.API.CustomMiddlewares;
+
+public class RequestTimeLoggingMiddleware(ILogger<RequestTimeLoggingMiddleware> logger) : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        var watch = Stopwatch.StartNew();
+        await next.Invoke(context);
+        watch.Stop();
+
+        if (watch.ElapsedMilliseconds / 1000 > 4 )
+        {
+            logger.LogWarning("Request [{Method}] at [{Path}] took [{ElapsedMilliseconds}] ms",
+                context.Request.Method,
+                context.Request.Path,
+                watch.ElapsedMilliseconds);
+        }
+
+    }
+}
