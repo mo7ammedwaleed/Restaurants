ac1e1101a5e587f7ab1f1e6f23f19478be14b29c
Mohammed Waleed <mwaleedx1@gmail.com>
2025-10-16 18:09:59 +0300

    User Context

--- Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Application.User;
 
 namespace Restaurants.Application.Extensions
 {
@@ -13,6 +14,9 @@ namespace Restaurants.Application.Extensions
             services.AddAutoMapper(applicationAssembly);
             services.AddValidatorsFromAssembly(applicationAssembly)
                 .AddFluentValidationAutoValidation();
+
+            services.AddScoped<IUserContext, UserContext>();
+            services.AddHttpContextAccessor();
         }
     }
 }
---
+++ Restaurants.Application/User/UserContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace Restaurants.Application.User;
+
+public class UserContext(IHttpContextAccessor httpContextAccessor) : IUserContext
+{
+    public CurrentUser? GetCurrentUser()
+    {
+        var user = httpContextAccessor?.HttpContext?.User;
+        if (user == null)
+        {
+            throw new InvalidOperationException("User context is not present");
+        }
+        if (user.Identity == null || !user.Identity.IsAuthenticated)
+        {
+            return null;
+        }
+        var userId = user.FindFirst(e => e.Type == ClaimTypes.NameIdentifier)!.Value;
+        var email = user.FindFirst(e => e.Type == ClaimTypes.Email)!.Value;
+        var roles = user.Claims.Where(e => e.Type == ClaimTypes.Role)!.Select(e => e.Value);
+
+        return new CurrentUser(userId, email, roles);
+
+    }
+}
