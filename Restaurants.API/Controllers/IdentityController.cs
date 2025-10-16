dd72b1579a6da96992ea0fbb73fe6d9903a74ed6
Mohammed Waleed <mwaleedx1@gmail.com>
2025-10-16 19:13:14 +0300

    Extending user definition

---
+++ Restaurants.API/Controllers/IdentityController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Users.Commands;
+
+namespace Restaurants.API.Controllers
+{
+    [Route("api/identity")]
+    [ApiController]
+    public class IdentityController(IMediator mediator) : ControllerBase
+    {
+        [HttpPatch("user")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [Authorize]
+        public async Task<IActionResult> UpdateUserDetails(UpdateUserDetailsCommand command)
+        {
+            await mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
