5e5142c57faf4a9f80a8e57feee3e0bdd59e5b72
Mohammed Waleed <mwaleedx1@gmail.com>
2025-11-06 09:56:55 +0200

    Pagination request validation

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
+
+public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+{
+    private int[] allowedMaxPageSize = [5,10,15,30];
+    public GetAllRestaurantsQueryValidator()
+    {
+        RuleFor(x => x.PageNumber)
+            .GreaterThanOrEqualTo(1);
+
+        RuleFor(x => x.PageSize)
+            .Must(pageSize => allowedMaxPageSize.Contains(pageSize))
+            .WithMessage($"PageSize must be in  {string.Join(", ", allowedMaxPageSize)}");
+
+
+    }
+
+}
