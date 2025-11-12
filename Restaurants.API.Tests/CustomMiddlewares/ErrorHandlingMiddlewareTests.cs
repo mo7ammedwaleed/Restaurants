64dec1929834c266d6daa3b390cba56bcac4c75d
Mohammed Waleed <mwaleedx1@gmail.com>
2025-11-12 08:34:46 +0200

    Middleware tests

---
+++ Restaurants.API.Tests/CustomMiddlewares/ErrorHandlingMiddlewareTests.cs
@@ -0,0 +1,89 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Exceptions;
+using Xunit;
+
+namespace Restaurants.API.CustomMiddlewares.Tests;
+
+public class ErrorHandlingMiddlewareTests
+{
+    [Fact()]
+    public async Task InvokeAsync_WhenNoExceptionThrown_ShouldCallNextDelegate()
+    {
+        // Arrange
+
+        var loggerMock = new Mock<ILogger<ErrorHandlingMiddleware>>();
+        var middleware = new ErrorHandlingMiddleware(loggerMock.Object);
+        var context = new DefaultHttpContext();
+        var nextDelegateMock = new Mock<RequestDelegate>();
+
+        // Act
+
+        await middleware.InvokeAsync(context, nextDelegateMock.Object);
+
+        // Assert
+
+        nextDelegateMock.Verify(next => next.Invoke(context), Times.Once);
+    }
+
+    [Fact()]
+    public async Task InvokeAsync_WhenNotFoundExceptionThrown_ShouldSetStatusCodeTo404AndWriteExceptionMessage()
+    {
+        // Arrange
+
+        var loggerMock = new Mock<ILogger<ErrorHandlingMiddleware>>();
+        var middleware = new ErrorHandlingMiddleware(loggerMock.Object);
+        var context = new DefaultHttpContext();
+        var notFoundException = new NotFoundException(nameof(Restaurant), "1");
+
+        // Act
+
+        await middleware.InvokeAsync(context, _ => throw notFoundException);
+
+        // Assert
+
+        context.Response.StatusCode.Should().Be(404);
+    }
+
+    [Fact()]
+    public async Task InvokeAsync_WhenForbiddenExceptionThrown_ShouldSetStautsCodeTo403()
+    {
+        // Arrange
+
+        var loggerMock = new Mock<ILogger<ErrorHandlingMiddleware>>();
+        var middleware = new ErrorHandlingMiddleware(loggerMock.Object);
+        var context = new DefaultHttpContext();
+        var forbiddenException = new ForbiddenException();
+
+        // Act
+
+        await middleware.InvokeAsync(context, _ => throw forbiddenException);
+
+        // Assert
+
+        context.Response.StatusCode.Should().Be(403);
+    }
+
+    [Fact()]
+    public async Task InvokeAsync_WhenGenericExceptionThrown_ShouldSetStautsCodeTo500()
+    {
+        // Arrange
+
+        var loggerMock = new Mock<ILogger<ErrorHandlingMiddleware>>();
+        var middleware = new ErrorHandlingMiddleware(loggerMock.Object);
+        var context = new DefaultHttpContext();
+        var exception = new Exception();
+
+        // Act
+
+        await middleware.InvokeAsync(context, _ => throw exception);
+
+        // Assert
+
+        context.Response.StatusCode.Should().Be(500);
+    }
+}
