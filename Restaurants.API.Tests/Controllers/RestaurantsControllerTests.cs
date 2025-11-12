421098fc030ad5e05c751c4900f782129f1999d2
Mohammed Waleed <mwaleedx1@gmail.com>
2025-11-12 09:06:20 +0200

    Controller integration tests

---
+++ Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+
+namespace Restaurants.API.Controllers.Tests;
+
+public class RestaurantsControllerTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+    public RestaurantsControllerTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact()]
+    public async Task GetAll_ForValidRequest_ShouldReturns200Ok()
+    {
+        // Arrange
+
+        var client = _factory.CreateClient();
+
+        // Act
+
+        var result = await client.GetAsync("/api/restaurants?pageNumber=1&pageSize=10");
+
+        // Assert
+
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact()]
+    public async Task GetAll_ForInValidRequest_ShouldReturns400BadRequest()
+    {
+        // Arrange
+
+        var client = _factory.CreateClient();
+
+        // Act
+
+        var result = await client.GetAsync("/api/restaurants");
+
+        // Assert
+
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+}
